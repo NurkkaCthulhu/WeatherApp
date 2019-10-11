fa8e5c1d44acb65e53672695d9ccd5aeb82a2777
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-10-11 13:30:38 +0300

    Add ForecastViewController.

---
+++ Weather App/ForecastViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ForecastViewController.swift
+//  Weather App
+//
+//  Created by Anu Malm on 11/10/2019.
+//  Copyright © 2019 Anu Malm. All rights reserved.
+//
+
+import UIKit
+
+class ForecastViewController: UIViewController {
+
+    @IBOutlet weak var forecastTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
