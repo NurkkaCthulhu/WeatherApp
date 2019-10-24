9020f1b0a87efabeb030f5f5b9a7927381526d9b
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-10-24 13:38:37 +0300

    Move location stuff to AppDelegate.

--- Weather App/AppDelegate.swift
+++ Weather App/AppDelegate.swift
@@ -7,15 +7,22 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
 
     var window : UIWindow?
+    
+    var locationManager : CLLocationManager!
+    var geocoder = CLGeocoder()
+    
     var locationWeather : LocationWeatherModel!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         locationWeather = LocationWeatherModel()
+        
+        determineMyCurrentLocation()
         
         return true
     }
@@ -33,6 +40,54 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
+    }
+    
+    // vv LOCATION RELATED FUNCTIONS vv
+    func determineMyCurrentLocation() {
+        print("koitetaa deteminecurrentlocation")
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations.last
+        
+        //self.meetingCostModel.latitude = (userLocation?.coordinate.latitude)!
+        //self.meetingCostModel.longitude = (userLocation?.coordinate.longitude)!
+        print("locationmanager functio \(String(describing: userLocation?.coordinate.latitude))!")
+        print("locationmanager functio \(String(describing: userLocation?.coordinate.longitude))!")
+        
+        addLocationToField()
+        
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func addLocationToField() {
+        let location = CLLocation(latitude: 37.785834, longitude: -122.406417)
+        print("add location to field")
+        // Start reversing the location lat/long into a city
+        geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            if let error = error {
+                print("Unable to Reverse Geocode Location (\(error))")
+            } else {
+                if let placemarks = placemarks, let placemark = placemarks.first {
+                    print("You are in \(placemark.locality!)")
+                } else {
+                    //self.meetingName.text = "No Matching Addresses Found"
+                }
+            }
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error \(error)")
     }
 
 
--- Weather App/CityViewController.swift
+++ Weather App/CityViewController.swift
@@ -7,12 +7,10 @@
 //
 
 import UIKit
-import CoreLocation
 
-class CityViewController: UIViewController, CLLocationManagerDelegate {
+
+class CityViewController: UIViewController {
     
-    var locationManager : CLLocationManager!
-    var geocoder = CLGeocoder()
     var locationWeather : LocationWeatherModel!
     
     // UI links
@@ -22,50 +20,6 @@ class CityViewController: UIViewController, CLLocationManagerDelegate {
         super.viewDidLoad()
     }
     
-    // vv LOCATION RELATED FUNCTIONS vv
-    func determineMyCurrentLocation() {
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
-        
-        locationManager.requestAlwaysAuthorization()
-        
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.startUpdatingLocation()
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let userLocation = locations.last
-        
-        //self.meetingCostModel.latitude = (userLocation?.coordinate.latitude)!
-        //self.meetingCostModel.longitude = (userLocation?.coordinate.longitude)!
-        
-        addLocationToField()
-        
-        self.locationManager.stopUpdatingLocation()
-    }
-    
-    func addLocationToField() {
-        let location = CLLocation(latitude: 2, longitude: 2)
-        
-        // Start reversing the location lat/long into a city
-        geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
-            if let error = error {
-                print("Unable to Reverse Geocode Location (\(error))")
-            } else {
-                if let placemarks = placemarks, let placemark = placemarks.first {
-                    //self.meetingName.text = "Meeting in \(placemark.locality!)"
-                } else {
-                    //self.meetingName.text = "No Matching Addresses Found"
-                }
-            }
-        }
-
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Error \(error)")
-    }
     
     // vv ALERT FUNCTION vv
     func showAlert(userInput : String) {
