11a84a95d18da569ce455fc314279f90392c8f12
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-10-25 13:49:20 +0300

    Add table view cell related code.

--- Weather App/ForecastViewController.swift
+++ Weather App/ForecastViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ForecastViewController: UIViewController {
+class ForecastViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var locationWeather : LocationWeatherModel!
     
@@ -17,6 +17,42 @@ class ForecastViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.forecastTable.dataSource = self
+        self.forecastTable.delegate = self
+        
+    }
+    
+    // Determine what happens when a cell is clicked
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("moi")
+    }
+    
+    // Determine how many cells are created
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locationWeather.temperatureList.count
+    }
+    
+    // Add cells to table view
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "forecastCell")
+        
+        if (cell == nil) {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "forecastCell")
+        }
+        cell!.textLabel?.text = "moikka"
+        cell!.imageView?.image = UIImage(named: locationWeather.temperatureList[indexPath.row].icon)
+        // Set the cell so that the cell does not look selected even if clicked
+        cell!.selectionStyle = UITableViewCell.SelectionStyle.none
+        
+        return cell!
+    }
+    
+    // Set selected location to show selected on table view
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if cell.textLabel!.text == self.locationWeather.city {
+            tableView.selectRow(at: indexPath, animated: false, scrollPosition: .none)
+        }
     }
     
 }
